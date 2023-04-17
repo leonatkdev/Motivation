85d05eaadd336c40259594187b265643b6d7bc6a
leonatk <leonatkdev@gmail.com>
2023-04-17 15:47:34 +0200

    fix: first screen

--- app/components/Views/Intro.js
+++ app/components/Views/Intro.js
@@ -1,10 +1,74 @@
-import React from 'react'
-import { View } from 'react-native'
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Image,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity
+} from "react-native";
+import data from "../../../constants/introData";
 
 const Intro = () => {
+  const [number, setNumber] = useState(0);
   return (
-    <View>Intro</View>
-  )
-}
+    <SafeAreaView>
+      <View style={styles.container}>
+        <Image
+          source={{
+            uri: data[number]?.imgURL,
+          }}
+          style={{ height: "100%" }}
+        />
+        <View
+          style={styles.box}
+          blurType="light"
+          blurAmount={10}
+          reducedTransparencyFallbackColor="white"
+        >
+          <Text style={styles.title}>{data[number]?.title}</Text>
+          <Text style={styles.desc}>{data[number]?.desc}</Text>
+          <View>
+            <View style={styles.dotsContainer}>
+              <FlatList
+                data={data}
+                renderItem={({ item, index }) => {
+                  const isActive = index === number;
+                  return (
+                    <TouchableOpacity onPress={() => setNumber(index) } style={[styles.dot, isActive && styles.activeDot]} />
+                  );
+                }}
+                horizontal={true}
+                keyExtractor={(_, index) => index.toString()}
+              />
+            </View>
+            <Image />
+          </View>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
 
-export default Intro
+const styles = StyleSheet.create({
+  container: { position: "relative" },
+  box: { position: "absolute", bottom: 16, left: 30, padding: 20 },
+  title: { color: "white", fontSize: 35 },
+  desc: { color: "white", fontSize: 20, paddingTop: 16 },
+  dotsContainer: { paddingTop: 40},
+  dot: {
+    width: 8,
+    height: 8,
+    borderRadius: 4,
+    backgroundColor: "#5E6165",
+    marginHorizontal: 6,
+  },
+  activeDot: {
+    backgroundColor: "white",
+    width: 9,
+    height: 9,
+  },
+});
+
+export default Intro;
---
+++ constants/introData.js
@@ -0,0 +1,28 @@
+const data = [
+  {
+    title: "Be You",
+    desc: 'Today, we being ourselfs.',
+    imgURL:
+      "https://i.pinimg.com/originals/94/28/17/942817465432d3c81c1c5d71c8a131e6.gif",
+  },
+  {
+    title: "Be You",
+    desc: 'Today, we being ourselfs.',
+    imgURL:
+      "https://64.media.tumblr.com/6f0a3cc72c4f5d64c201270a5d09647d/tumblr_pfxdeySS8j1wo2a1ao1_500.gifv",
+  },
+  {
+    title: "Be You",
+    desc: 'Today, we being ourselfs.',
+    imgURL:
+      "https://64.media.tumblr.com/7482d3a1914f545045d3394c97dda98b/tumblr_p5vqokJaDi1wo2a1ao1_500.gifv",
+  },
+  {
+    title: "Be You",
+    desc: 'Today, we being ourselfs.',
+    imgURL:
+      "https://64.media.tumblr.com/099dcc3fa60717936ab191e9f0035ef8/4ecc33dee91c7678-b0/s1280x1920/cb1cfa62d0a229617cf0207e30dd228eab6aa47d.jpg",
+  },
+];
+
+export default data;
